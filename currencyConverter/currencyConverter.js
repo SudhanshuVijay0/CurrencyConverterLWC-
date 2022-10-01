f0f36c0ed1fc74f0442bdccd5d0df542b1bc0e9e
SudhanshuVijay0 <78337703+SudhanshuVijay0@users.noreply.github.com>
2022-10-01 11:50:49 +0530

    First push

---
+++ currencyConverter/currencyConverter.js
@@ -0,0 +1,80 @@
+import { LightningElement, track, wire } from 'lwc';
+import getconversion from '@salesforce/apex/currencyConverterController.getConversion';
+import getCodes from '@salesforce/apex/currencyConverterController.getCodes';
+export default class CurrencyConverter extends LightningElement 
+{
+    @track convertFrom='';
+    @track convertTo='';
+    @track amount='';
+    @track result='';
+    codes;
+    valueFrom = '';
+    valueTo = '';
+    options=[];
+    
+    @wire(getCodes,{})
+    wiredData({ error, data }) {
+      if (data) {
+            try{
+                this.codes = data;
+                console.log('data-->', this.codes)
+                let option = [];
+                var keys = Object.keys(data);
+                keys.forEach(currentItem => {
+                    var obj = {};
+                    obj['label'] = data[currentItem]['description'];
+                    obj['value'] = currentItem;
+                    option.push(obj);
+                });
+                this.options = option;
+            }
+            catch (error) {
+                console.error('check error here', error);
+            }
+        
+      } else if (error) {
+        console.error('Error:', error);
+      }
+    }
+    handleClick()
+    {
+        getconversion({amount:this.amount,convertFrom: this.convertFrom, convertTo: this.convertTo})
+        .then((res) => {
+            this.result = res.toString()
+            console.log(this.result);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
+    handleFromChange(event)
+    {
+        console.log(event.target.value)
+        this.convertFrom = event.target.value;
+    }
+
+    handleToChange(event)
+    {
+        console.log(event.target.value)
+        this.convertTo = event.target.value;
+    }
+
+    handleAmountChange(event)
+    {
+        console.log(event.target.value)
+        this.amount = event.target.value;
+    }
+
+    handleClear()
+    {
+        this.amount = '';
+        this.convertFrom = '';
+        this.convertTo = '';
+        this.result = '';
+    }
+
+    handleChange(event)
+    {
+        this.value = event.target.value;
+    }
+}
